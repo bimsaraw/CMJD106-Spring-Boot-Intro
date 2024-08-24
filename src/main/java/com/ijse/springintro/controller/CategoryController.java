42194fe372deb7e66473c9ea907c0359ef08ca92
Bimsara Walallawita <bwalallawita@gmail.com>
2024-08-24 11:00:35 +0530

    CORS fix

--- src/main/java/com/ijse/springintro/controller/CategoryController.java
+++ src/main/java/com/ijse/springintro/controller/CategoryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import com.ijse.springintro.entity.Category;
 import com.ijse.springintro.service.CategoryService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class CategoryController {
     
     @Autowired
--- src/main/java/com/ijse/springintro/controller/ProductController.java
+++ src/main/java/com/ijse/springintro/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import com.ijse.springintro.service.CategoryService;
 import com.ijse.springintro.service.ProductService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ProductController {
     
     @Autowired
